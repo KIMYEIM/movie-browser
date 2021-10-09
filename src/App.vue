f8053b428a688f12950385944c25fffb05569914
KIMYEIM <kyaeim@gmail.com>
2021-10-09 23:22:30 +0900

    ✨  feat: Footer 생성, 전체 페이지 레이아웃

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,12 @@
 <template>
-  <router-view />
+  <div class="app__page">
+    <router-view />
+  </div>
 </template>
+
+<style lang="scss" scoped>
+  .app__page {
+    height: 100vh;
+    font-family: 'Roboto', sans-serif;
+  }
+</style>
---
+++ src/components/Footer.vue
@@ -0,0 +1,25 @@
+<template>
+  <footer>
+    <h1>{{location}}</h1>
+  </footer>
+</template>
+
+<script>
+export default {
+  props: {
+    location: '',
+  }
+}
+</script>
+
+<style scoped lang="scss">
+  footer {
+    height: 87px;
+    border-top: 1px solid black;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-family: 'Source Serif Pro', serif;
+    font-size: 24px;
+  }
+</style>
