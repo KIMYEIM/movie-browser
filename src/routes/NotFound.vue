d166feb16148282901232265b34548e086f13355
KIMYEIM <kyaeim@gmail.com>
2021-10-09 23:52:24 +0900

    ✨ feat: router 세팅, NotFound 페이지

---
+++ src/routes/NotFound.vue
@@ -0,0 +1,29 @@
+<template>
+  <section>
+    <h1>404 Page Not Found!</h1>
+    <button
+      @click="goHome"
+    >Go to Home</button>
+  </section>
+</template>
+
+<script>
+export default {
+  methods: {
+    goHome() {
+      this.$router.replace('/')
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  section {
+    padding: 100px 0 200px;
+    text-align: center;
+    h1 {
+      font-size: 60px;
+      font-weight: 700;
+    }
+  }
+</style>
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,13 +1,24 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import Home from './Home';
+import Movies from './Movies';
+import NotFound from './NotFound';
 
 export default createRouter({
-  history: createWebHistory(), 
-  scrollBehavior: () => ({top: 0}),
+  history: createWebHistory(),
   routes: [
     {
+      name: 'Home',
       path: '/',
-      component: Home
-    }
-  ]
+      component: Home,
+    },
+    {
+      name: 'Movies',
+      path: '/movies/:title',
+      component: Movies,
+    },
+    {
+      path: '/:notFound(.*)',
+      component: NotFound,
+    },
+  ],
 });
