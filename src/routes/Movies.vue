575eb1c886b0b23820103f6973e1cd0458fca9f6
KIMYEIM <kyaeim@gmail.com>
2021-10-10 00:24:15 +0900

    ✨ feat: Movies page 레이아웃

---
+++ src/routes/Movies.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="movies__container">
+    <header class="movies__header">
+      <span class="material-icons">
+        chevron_left
+      </span>
+      <input
+      v-focus
+      ref="input"
+      class="header__input"
+      :value="this.nowTitle"
+      />
+    </header>
+    <section class="movies__movies">
+
+    </section>
+    <Footer location="search"/>
+  </div>
+</template>
+
+<script>
+import Footer from '~/components/Footer'
+export default {
+  components: {
+    Footer,
+  },
+  directives: {
+    focus: {
+      mounted(el) {
+        el.focus()
+        }
+    }
+  },
+  computed: {
+    nowTitle() {
+      return this.$route.params.title;
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  .movies__container {
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    .movies__header {
+      padding-left: 40px;
+      display: flex;
+      align-items: center;
+      height: 147px;
+      border-bottom: 1px solid black;
+      input {
+        margin-left: 30px;
+        text-transform: uppercase;
+        font-size: 64px;
+        border: none;
+        outline: none;
+      }
+      span {
+        font-size: 64px;
+      }
+    }
+    .movies__movies {
+      flex-grow: 1;
+    }
+  }
+</style>
