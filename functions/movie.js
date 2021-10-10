d79ed039760d5d8bdf33d110127c74d75806461f
KIMYEIM <kyaeim@gmail.com>
2021-10-10 21:41:23 +0900

    🔧 update serverless function

--- functions/movie.js
+++ functions/movie.js
@@ -4,9 +4,10 @@ const { API_ENDPOINT, API_KEY } = process.env
 
 exports.handler = async function (event) {
   const options = JSON.parse(event.body)
-  const { title='' } = options
+  const { title, id, type} = options;
+  let param = type === 'movies' ? `&s=${title}` : `&i=${id}&plot=short`; 
   const { data } = await axios({
-    url: `${API_ENDPOINT}?apikey=${API_KEY}&s=${title}`,
+    url: `${API_ENDPOINT}?apikey=${API_KEY}${param}`,
     method: 'GET',
   });
   return {
